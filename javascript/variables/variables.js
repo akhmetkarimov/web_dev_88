23a7a29676f594ef42088508e302537812544912
Yedil Akhmetkarimov <yedil.akhmetkarimov@aspose.com>
2020-12-26 21:04:17 +0600

    26.12.2020 javascript

---
+++ javascript/variables/variables.js
@@ -0,0 +1,100 @@
+// alert('Hello external')
+console.log('Hello external console')
+
+document.write('<h1>Hello Write</h1>')
+
+document.getElementById('changeText').innerHTML = 'Decode <a href="https://decode.kz">Link</a>'
+
+// var, let, const
+
+// var/let/const variableName = 20/'hello'/true
+
+console.log(myVar)
+
+var myVar = 10
+
+console.log(myVar)
+
+myVar = 50
+
+console.log(myVar)
+
+
+let myLet
+
+// myLet = ÷20
+
+console.log(myLet)
+
+
+const myConst = 600
+
+console.log(myConst)
+
+
+let sum = 20 + 10 // 30
+let sub = 20 - 10 // 10
+let qwe = 20 * 10
+let div = sum / sub // 30 / 10
+let mod = 7 % 2
+
+console.log(sum)
+console.log(sub)
+console.log(qwe)
+console.log(div)
+console.log(mod)
+
+console.log(4 % 2)
+console.log(3 % 2)
+
+
+let first = 50
+let second = 10
+
+console.log(first, second)
+
+// let third = first // 50
+
+// first = second // 10 
+// second = third // 50
+
+first = first + second // 60
+second = first - second // 60 - 10 = 50
+first = first - second // 60 - 50 = 10
+
+console.log(first, second)
+
+// let myName = 'hello'
+// myName = prompt('Enter name')
+
+// console.log('Привет ' + myName)
+
+
+// let num1 = parseInt(prompt('Enter number 1'))
+// let num2 = parseInt(prompt('Enter number 2'))
+
+// let num1 = Number(prompt('Enter number 1'))
+// let num2 = Number(prompt('Enter number 2'))
+
+let num1 = +(prompt('Enter number 1'))
+let num2 = +(prompt('Enter number 2'))
+
+console.log('Sum num1 + num2 = ', num1 + num2)
+
+
+// Создайте переменную str и присвойте ей значение 'Привет, Мир!'. Выведите значение этой переменной на экран.
+
+
+// Создайте переменные str1='Привет, ' и str2='Мир!'. С помощью этих переменных и операции сложения строк выведите на экран фразу 'Привет, Мир!'.
+
+
+// Напишите алгоритм, который считает количество секунд в часе.
+
+
+// Создайте переменные a=10 и b=2. Выведите на экран их сумму, разность, произведение и частное (результат деления).
+
+
+// Создайте переменные c=15 и d=2. Просуммируйте их, а результат присвойте переменной result. Выведите на экран значение переменной result.
+
+
+// Создайте переменные a=10, b=2 и c=5. Выведите на экран их сумму.
