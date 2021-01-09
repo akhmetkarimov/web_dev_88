a0036050824b90acdb24fc93246c905212ca21e2
Yedil Akhmetkarimov <yedil.akhmetkarimov@aspose.com>
2021-01-09 20:57:15 +0600

    09.01.2021 loops

---
+++ javascript/loops/loops.js
@@ -0,0 +1,114 @@
+// while (condition) {
+    // body
+// }
+
+// if (condition) {
+//     body
+// }
+
+// let counter = 1
+// let sumEven = 0
+
+// while (counter <= 5) {
+//     let num =+prompt('enter number')
+
+//     if (num % 2 == 0) {
+//         sumEven += num
+//     }
+
+//     counter += 1
+// }
+
+// console.log(sumEven)
+
+
+// let randomNumber = Math.floor(Math.random() * 10)
+// let answer = 0
+// let attept = 0
+
+// let win = false
+
+// while (attept < 3) {
+//     answer = +prompt('Enter guess number')
+
+//     if (answer > randomNumber) {
+//         alert('You need enter less number')
+//     } else if (answer < randomNumber) {
+//         alert('You need enter high number')
+//     } else if (answer === randomNumber) {
+//         win = true
+//         break
+//     }
+    
+//     attept += 1
+// }
+
+// if (win == false) {
+//     alert('You lose')   
+// } else {
+//     alert('You win')
+// }
+
+
+// let username = ""
+// let failed = -1
+
+// while (username !== 'admin') {
+//     username = prompt('Enter username')
+//     failed += 1
+// }
+
+// alert('Welcome admin')
+// console.log('Failed attempts:',failed)
+
+
+// for (start; condition; action) {
+//     body
+// }
+
+// for (let i = 0; i < 12; i += 1) {
+//     document.write('<div class = "box"></div>')
+// }
+
+
+// do {
+//     console.log('Do while')
+// } while (20 == 10)
+
+
+// let number = 61238314 // 28
+// let sum = 0
+// let counter = 0
+
+// while (number > 0) {
+//     counter += 1
+//     sum += number % 10
+//     number = Math.floor(number / 10)
+// }
+
+// console.log(sum)
+
+
+
+// for (start; condition; action) {
+//     body
+// }
+
+
+// let sum = 0
+
+// for (let number = 5812341; number > 0; number = Math.floor(number / 10)) {
+//     sum += number % 10
+// }
+
+// console.log(sum)
+
+// let num = 123
+// let counter = 0
+
+// while (num > 0) {
+//     counter += 1
+//     num = Math.floor(num / 10)
+// }
+
+// console.log(counter)
